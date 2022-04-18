deb7ed4fb5bab273b8f50776bcd7f1b022f6ea6f
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-04-18 14:37:31 -0600

    connected to state

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 
-import reducer from '../reducers';
+import reducer, { initialState } from '../reducers';
 
 function App() {
 
@@ -20,10 +20,10 @@ const [state, dispatch] = useReducer(reducer, initialState);
         <div className="col-md-12 d-flex justify-content-center">
           <form name="Cal">
             
-            <TotalDisplay value={0}/>
+            <TotalDisplay value={state.total}/>
             <div className="row details">
-              <span id="operation"><b>Operation:</b> X</span>
-              <span id="memory"><b>Memory:</b> 0</span>
+              <span id="operation"><b>Operation:</b> {state.operation} </span>
+              <span id="memory"><b>Memory:</b> {state.memory}</span>
             </div>
             
             <div className="row">
